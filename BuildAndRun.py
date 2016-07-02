cd4d061d4cf92895382abb29b31b3b3d8ee7f9c0
Simon Tucker <brotherlogic@gmail.com>
2016-07-02 16:39:51 -0700

    Fix for build script

--- BuildAndRun.py
+++ BuildAndRun.py
@@ -1,12 +1,17 @@
 import os
 import subprocess
 
-# Update to the latest version
-for line in os.popen('git fetch -p -q; git merge -q origin/master').readlines():
-    print line.strip()
+name = "discovery"
 
+current_hash = os.popen('git rev-parse HEAD').readlines()[0]
+# Update to the latest version
+for line in os.popen('go get -u github.com/brotherlogic/' + name).readlines():
+    print line.strip()
+new_hash = os.popen('git rev-parse HEAD').readlines()[0]
+
+    
 # Move the old version over
-for line in os.popen('cp discovery olddiscovery').readlines():
+for line in os.popen('cp ' + name + ' old' + name).readlines():
     print line.strip()
 
 # Rebuild
@@ -17,11 +22,13 @@ for line in os.popen('go build ./...').readlines():
 for line in os.popen('go build').readlines():
     print line.strip()
 
+size_1 = os.path.getsize('./old' + name)
+size_2 = os.path.getsize('./' + name)
 
-size_1 = os.path.getsize('./discovery')
-size_2 = os.path.getsize('./olddiscovery')
+running = len(os.popen('ps -ef | grep ' + name).readlines()) > 3
 
-if size_1 != size_2:
-    for line in os.popen('killall discovery').readlines():
+              
+if size_1 != size_2 or new_hash != current_hash or not running:
+    for line in os.popen('killall ' + name).readlines():
         pass
-    subprocess.Popen('./discovery')
+    subprocess.Popen(['./' + name, '--sync=false'])
