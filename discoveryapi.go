9293a6f9ab2c6197584ceb35215a274653ab790a
Simon Tucker <brotherlogic@gmail.com>
2017-05-27 11:07:22 -0700

    Added insecure to dial option

--- discoveryapi.go
+++ discoveryapi.go
@@ -17,7 +17,7 @@ type prodHealthChecker struct{}
 
 func (healthChecker prodHealthChecker) Check(entry *pb.RegistryEntry) bool {
 	log.Printf("Dialing for health: %v", entry)
-	conn, err := grpc.Dial(entry.Ip+":"+strconv.Itoa(int(entry.Port)), grpc.WithTimeout(time.Second*5))
+	conn, err := grpc.Dial(entry.Ip+":"+strconv.Itoa(int(entry.Port)), grpc.WithTimeout(time.Second*5), grpc.WithInsecure())
 	if err != nil {
 		log.Printf("Can't event dial %v -> %v", entry, err)
 		return false
