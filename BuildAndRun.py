cc3a7a8e9b89d5e97ef689c8fb37b77d725c5746
Simon Tucker <brotherlogic@gmail.com>
2017-06-17 07:26:27 -0700

    Added crash reports

--- BuildAndRun.py
+++ BuildAndRun.py
@@ -28,6 +28,8 @@ running = len(os.popen('ps -ef | grep ' + name).readlines()) > 3
 
               
 if size_1 != size_2 or new_hash != current_hash or not running:
+    for line in os.popen('cat out.txt | mail -s "Crash Report ' + name + '" brotherlogic@gmail.com').readlines():
+        pass
     for line in os.popen('killall ' + name).readlines():
         pass
     subprocess.Popen(['./' + name, '--sync=false'])
