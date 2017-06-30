e95627dd3762f60d540f3ef99edc68353f2f1f83
Simon Tucker <brotherlogic@gmail.com>
2017-06-29 19:09:29 -0700

    Fixed double counting. This closes #60

--- discovery.go
+++ discovery.go
@@ -108,69 +108,69 @@ func (s *Server) ListAllServices(ctx context.Context, in *pb.Empty) (*pb.Service
 func (s *Server) RegisterService(ctx context.Context, in *pb.RegistryEntry) (*pb.RegistryEntry, error) {
 	log.Printf("Registering %v", in)
 
-	//Don't register things that are already registered
-	if in.Port <= 0 {
-		// Server is requesting an external port
-		if in.ExternalPort {
-			availablePorts := externalPorts["main"]
-			// Reset the request IP to an external IP
-			in.Ip = s.getExternalIP(prodHTTPGetter{})
+	// Server is requesting an external port
+	if in.ExternalPort {
+		availablePorts := externalPorts["main"]
+		// Reset the request IP to an external IP
+		in.Ip = s.getExternalIP(prodHTTPGetter{})
 
-			for _, port := range availablePorts {
-				log.Printf("Assigning port number for external %v", port)
-				taken := false
-				for _, service := range s.entries {
-					if service.Ip == in.Ip && service.Port == port {
-						taken = true
-					}
-					// If we've already registered this service, return immediately
-					if service.Identifier == in.Identifier && service.Name == in.Name {
-						//Refresh the IP and store the checkfile
-						service.Ip = in.Ip
-						log.Printf("Fast return : %v", service)
-						s.saveCheckFile()
-						log.Printf("Returning quick")
-						return service, nil
-					}
+		for _, port := range availablePorts {
+			log.Printf("Assigning port number for external %v", port)
+			taken := false
+			for _, service := range s.entries {
+				if service.Ip == in.Ip && service.Port == port {
+					taken = true
 				}
-
-				if !taken {
-					in.Port = port
-					break
+				// If we've already registered this service, return immediately
+				if service.Identifier == in.Identifier && service.Name == in.Name {
+					//Refresh the IP and store the checkfile
+					service.Ip = in.Ip
+					log.Printf("Fast return : %v", service)
+					s.saveCheckFile()
+					log.Printf("Returning quick")
+					return service, nil
 				}
 			}
 
-			//Throw an error if we can't find a port number
-			if in.Port <= 0 {
-				return in, errors.New("Unable to allocate external port")
+			if !taken {
+				in.Port = port
+				break
 			}
-		} else {
-			var portNumber int32
-			for portNumber = 50055 + 1; portNumber < 60000; portNumber++ {
-				log.Printf("Assigning port number %v", portNumber)
-				taken := false
-				for _, service := range s.entries {
-					if service.Port == portNumber {
-						taken = true
-					}
+		}
 
-					// If we've already registered this service, return immediately
-					if service.Identifier == in.Identifier && service.Name == in.Name {
-						//Refresh the IP and store the checkfile
-						service.Ip = in.Ip
-						s.saveCheckFile()
-						return service, nil
-					}
-
-					//Unmaster the service if the incoming also wants to be master
-					if service.Name == in.Name && service.Master && in.Master {
-						service.Master = false
-					}
+		//Throw an error if we can't find a port number
+		if in.Port <= 0 {
+			return in, errors.New("Unable to allocate external port")
+		}
+	} else {
+		var portNumber int32
+		for portNumber = 50055 + 1; portNumber < 60000; portNumber++ {
+			log.Printf("Assigning port number %v", portNumber)
+			taken := false
+			for _, service := range s.entries {
+				if service.Port == portNumber {
+					taken = true
 				}
-				if !taken {
+
+				// If we've already registered this service, return immediately
+				if service.Identifier == in.Identifier && service.Name == in.Name {
+					//Refresh the IP and store the checkfile
+					service.Ip = in.Ip
+					s.saveCheckFile()
+					return service, nil
+				}
+
+				//Unmaster the service if the incoming also wants to be master
+				if service.Name == in.Name && service.Master && in.Master {
+					service.Master = false
+				}
+			}
+			if !taken {
+				//Only set the port if it's not set
+				if in.Port <= 0 {
 					in.Port = portNumber
-					break
 				}
+				break
 			}
 		}
 	}
