7875770deca18851bb7555995c56d12c922fd2ba
Simon Tucker <simon@brotherlogic@gmail>
2019-11-24 09:25:46 -0800

    Full coverage

--- discoveryv3api_test.go
+++ discoveryv3api_test.go
@@ -31,6 +31,30 @@ func TestRegisterV2(t *testing.T) {
 	}
 }
 
+func TestRegisterV2NoFriend(t *testing.T) {
+	s := InitTestServer()
+
+	resp, err := s.RegisterV2(context.Background(), &pb.RegisterRequest{Service: &pb.RegistryEntry{Name: "test_job", Identifier: "test_server", Version: pb.RegistryEntry_V2}})
+
+	if err != nil {
+		t.Errorf("Unable to register %v", err)
+	}
+
+	if resp.Service.Port == 0 {
+		t.Errorf("Port number not assigned")
+	}
+
+	s.friends = append(s.friends, "madeup")
+	respg, err := s.Get(context.Background(), &pb.GetRequest{Job: "test_job", Server: "test_server", Friend: "madeup"})
+	if err != nil {
+		t.Errorf("Unable to get %v", err)
+	}
+
+	if len(respg.Services) != 1 {
+		t.Errorf("Service not returned")
+	}
+}
+
 func TestRegisterV2MasterFail(t *testing.T) {
 	s := InitTestServer()
 
