eea4d772c87ac3c0e12b12745930e7b365f12137
Simon Tucker <brotherlogic@gmail.com>
2020-04-15 12:34:11 -0700

    Builds out proper proto

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
 protoc --proto_path ../../../ -I=./proto --go_out=plugins=grpc:./proto proto/discovery.proto --js_out=import_style=commonjs:./proto  --grpc-web_out=import_style=commonjs,mode=grpcwebtext:./proto --dart_out=grpc:./proto
+mv proto/github.com/brotherlogic/discovery/proto/* ./proto
