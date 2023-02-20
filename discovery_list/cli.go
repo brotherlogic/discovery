c79955383aa49a135ba367cc3bb8e17ce34d173a
Simon Tucker <brotherlogic@gmail.com>
2023-02-20 11:18:35 -0800

    Adds cluster entries

--- discovery_list/cli.go
+++ discovery_list/cli.go
@@ -56,6 +56,9 @@ func main() {
 			// This is the raspberry pi export
 			nodes.Targets = append(nodes.Targets, fmt.Sprintf("%v:9100", server))
 		}
+		for val := 1; val <= 4; val++ {
+			nodes.Targets = append(nodes.Targets, fmt.Sprintf("kclust%v:9110", val))
+		}
 		nodes2 := &Entry{Targets: []string{}, Labels: Label{Job: "discovery"}}
 		for server, _ := range servers {
 
