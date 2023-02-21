a8f3520560e22cdae7bbd855dfe47fa2a84b8e05
Simon Tucker <brotherlogic@gmail.com>
2023-02-21 08:13:26 -0800

    Supports kube lookup for ingress. This closes #656

--- discoveryv2api.go
+++ discoveryv2api.go
@@ -3,14 +3,20 @@ package main
 import (
 	"context"
 	"errors"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 	"time"
 
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/peer"
 	"google.golang.org/grpc/status"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	pb "github.com/brotherlogic/discovery/proto"
 	"github.com/prometheus/client_golang/prometheus"
@@ -36,6 +42,44 @@ var (
 		Help: "The size of the print queue",
 	}, []string{"service", "origin"})
 )
+
+func (s *Server) getFromKube(ctx context.Context, req *pb.GetRequest) (*pb.GetResponse, error) {
+	// use the current context in kubeconfig
+	kubeconfig := flag.String("kubeconfig", filepath.Join("/home/simon/", ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		s.RaiseIssue("Missing kubeconfig", fmt.Sprintf("%v is missing the kube config", s.Registry.Identifier))
+	}
+
+	// create the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+
+	pods, err := clientset.CoreV1().Services("").List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		return nil, err
+	}
+	var services []*pb.RegistryEntry
+
+	for _, pod := range pods.Items {
+		if pod.Name == req.GetJob() || req.GetJob() == "" {
+			for _, port := range pod.Spec.Ports {
+				if port.Name == "grpc" && port.NodePort > 0 {
+					services = append(services, &pb.RegistryEntry{
+						Name:       pod.Name,
+						Identifier: "kclust1",
+						Port:       port.NodePort,
+						Zone:       "cluster",
+					})
+				}
+			}
+		}
+	}
+
+	return &pb.GetResponse{Services: services}, nil
+}
 
 func (s *Server) addIP(ip string) {
 	for _, lip := range s.iplist {
@@ -195,6 +239,15 @@ func (s *Server) Get(ctx context.Context, req *pb.GetRequest) (*pb.GetResponse, 
 			return &pb.GetResponse{Services: jobs, State: s.state}, nil
 		}
 
+		maybe, err := s.getFromKube(ctx, req)
+		if err != nil {
+			return nil, err
+		}
+
+		if len(maybe.Services) > 0 {
+			return maybe, err
+		}
+
 		return nil, status.Errorf(codes.Unavailable, "%v has not found on %v (via %v)", req.Job, req.Server, s.Registry.GetIdentifier())
 	}
 
@@ -205,6 +258,16 @@ func (s *Server) Get(ctx context.Context, req *pb.GetRequest) (*pb.GetResponse, 
 			resp.Services = append(resp.Services, job)
 		}
 	}
+
+	maybe, err := s.getFromKube(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(maybe.Services) > 0 {
+		resp.Services = append(resp.Services, maybe.Services...)
+	}
+
 	return resp, nil
 }
 
