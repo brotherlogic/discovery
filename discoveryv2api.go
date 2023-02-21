9723114c856256590efd239da80b0542d2cb87b5
Simon Tucker <brotherlogic@gmail.com>
2023-02-21 08:22:27 -0800

    Flag guard kube call

--- discoveryv2api.go
+++ discoveryv2api.go
@@ -239,13 +239,15 @@ func (s *Server) Get(ctx context.Context, req *pb.GetRequest) (*pb.GetResponse, 
 			return &pb.GetResponse{Services: jobs, State: s.state}, nil
 		}
 
-		maybe, err := s.getFromKube(ctx, req)
-		if err != nil {
-			return nil, err
-		}
+		if s.kube {
+			maybe, err := s.getFromKube(ctx, req)
+			if err != nil {
+				return nil, err
+			}
 
-		if len(maybe.Services) > 0 {
-			return maybe, err
+			if len(maybe.Services) > 0 {
+				return maybe, err
+			}
 		}
 
 		return nil, status.Errorf(codes.Unavailable, "%v has not found on %v (via %v)", req.Job, req.Server, s.Registry.GetIdentifier())
@@ -259,13 +261,15 @@ func (s *Server) Get(ctx context.Context, req *pb.GetRequest) (*pb.GetResponse, 
 		}
 	}
 
-	maybe, err := s.getFromKube(ctx, req)
-	if err != nil {
-		return nil, err
-	}
+	if s.kube {
+		maybe, err := s.getFromKube(ctx, req)
+		if err != nil {
+			return nil, err
+		}
 
-	if len(maybe.Services) > 0 {
-		resp.Services = append(resp.Services, maybe.Services...)
+		if len(maybe.Services) > 0 {
+			resp.Services = append(resp.Services, maybe.Services...)
+		}
 	}
 
 	return resp, nil
