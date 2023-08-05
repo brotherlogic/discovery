701073b99de0d432b6be86222e9cf2b9477276aa
Simon Tucker <brotherlogic@gmail.com>
2023-08-05 10:08:38 -0700

    Bounce

--- discoveryv2api.go
+++ discoveryv2api.go
@@ -195,7 +195,7 @@ func (s *Server) Get(ctx context.Context, req *pb.GetRequest) (*pb.GetResponse, 
 			return &pb.GetResponse{Services: jobs, State: s.state}, nil
 		}
 
-		return nil, status.Errorf(codes.Unavailable, "%v was not found on %v (via %v)", req.Job, req.Server, s.Registry.GetIdentifier())
+		return nil, status.Errorf(codes.Unavailable, "%v was not found on %v (via this %v)", req.Job, req.Server, s.Registry.GetIdentifier())
 	}
 
 	resp := &pb.GetResponse{Services: []*pb.RegistryEntry{}, State: s.state}
